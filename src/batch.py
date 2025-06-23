b43063c8b3f04f0fb82a89e303021672dd430962
Roman Baravalle <romanbaravalle@gmail.com>
2025-06-23 15:42:56 -0500

    BatchConfig

--- src/batch.py
+++ src/batch.py
@@ -811,11 +811,170 @@ def evolRates(popSize=30, maxGen=200, scaleDensity=1.0):
         'crossover': 0.5,
         'maximize': False, # maximize fitness function?
         'max_generations': maxGen,
-        'time_sleep': 300, # 5min wait this time before checking again if sim is completed (for each generation)
-        'maxiter_wait': 2*64, # (5h20) max number of times to check if sim is completed (for each generation)
+        'time_sleep': 10*60, # 5min wait this time before checking again if sim is completed (for each generation)
+        'maxiter_wait': 10*24, # (5h20) max number of times to check if sim is completed (for each generation)
         'defaultFitness': 1000, # set fitness value in case simulation time is over
     }
 
+
+    return b
+
+# ----------------------------------------------------------------------------------------------
+# Adaptive Stochastic Descent (ASD)
+# ----------------------------------------------------------------------------------------------
+def optunaRatesTotal(scaleDensity=1.0):
+
+    # --------------------------------------------------------
+    # parameters
+    params = specs.ODict()
+
+    # long-range inputs
+    params[('weightLong', 'TPO')] = [0.25, 0.75]
+    params[('weightLong', 'TVL')] = [0, 0.25] #[0.25, 0.75]
+    params[('weightLong', 'S1')] =  [0.25, 0.75]
+    params[('weightLong', 'S2')] =  [0.25, 0.75]
+    params[('weightLong', 'cM1')] = [0.25, 0.75]
+    params[('weightLong', 'M2')] =  [0.25, 0.75]
+    params[('weightLong', 'OC')] =  [0.25, 0.75]
+
+    # E->I gain per cell
+    params[('EICellTypeGain', 'PV')] = [0.1, 4.0]
+    params[('EICellTypeGain', 'SOM')] = [0.1, 4.0]
+    params[('EICellTypeGain', 'VIP')] = [0.1, 4.0]
+    params[('EICellTypeGain', 'NGF')] = [0.1, 4.0]
+
+    # EEgain
+    params['EEGain'] = [0.5, 1.5]
+
+    # IEgain
+    ## L2/3+4
+    params[('IEweights',0)] =  [0.5, 1.5]
+    ## L5
+    params[('IEweights',1)] = [0.5, 1.5] #[0.8, 1.0]   
+    ## L6
+    params[('IEweights',2)] =  [0.5, 1.5] # [0.8, 1.0]  
+
+    # IIGain
+    ## L2/3+4
+    params[('IIweights',0)] =  [0.5, 1.5]
+    ## L5
+    params[('IIweights',1)] = [0.5, 1.5] #[0.8, 1.0]   
+    ## L6
+    params[('IIweights',2)] =  [0.5, 1.5] # [0.8, 1.0]  
+
+    groupedParams = []
+
+    # --------------------------------------------------------
+    # initial config
+    initCfg = {}
+    initCfg['duration'] = 1500
+    initCfg['printPopAvgRates'] =  [500, 1500]
+    initCfg['dt'] = 0.025
+
+    initCfg['scaleDensity'] = scaleDensity
+
+    # cell params
+    initCfg['ihGbar'] = 0.75  # ih (for quiet/sponti condition)
+    initCfg['ihModel'] = 'migliore'  # ih model
+    initCfg['ihGbarBasal'] = 1.0 # multiplicative factor for ih gbar in PT cells
+    initCfg['ihlkc'] = 0.2 # ih leak param (used in Migliore)
+    initCfg['ihLkcBasal'] = 1.0 # multiplicative factor for ih lk in PT cells
+    initCfg['ihLkcBelowSoma'] = 0.01 # multiplicative factor for ih lk in PT cells
+    initCfg['ihlke'] = -86  # ih leak param (used in Migliore)
+    initCfg['ihSlope'] = 28  # ih leak param (used in Migliore)
+
+    initCfg['somaNa'] = 5.0  # somatic Na conduct
+    initCfg['dendNa'] = 0.3  # dendritic Na conduct (reduced to avoid dend spikes) 
+    initCfg['axonNa'] = 7   # axon Na conduct (increased to compensate) 
+    initCfg['axonRa'] = 0.005
+    initCfg['gpas'] = 0.5
+    initCfg['epas'] = 0.9
+
+    # long-range input params
+    initCfg['numCellsLong'] = 1000
+    initCfg[('pulse', 'pop')] = 'None'
+    initCfg[('pulse', 'start')] = 1000.0
+    initCfg[('pulse', 'end')] = 1100.0
+    initCfg[('pulse', 'noise')] = 0.8
+
+    # conn params
+    initCfg['IEdisynapticBias'] = None
+
+    initCfg['weightNormThreshold'] = 4.0
+    initCfg['IEGain'] = 1.0
+    initCfg['IIGain'] = 1.0
+    initCfg['IPTGain'] = 1.0
+
+    # plotting and saving params
+    initCfg[('analysis','plotRaster','timeRange')] = [500,1500]
+    # initCfg['recordTraces'] = None
+    # initCfg[('analysis', 'plotTraces')] = False
+    initCfg[('analysis', 'plotTraces', 'timeRange')] = [500,1500]
+    initCfg[('analysis', 'plotTraces', 'oneFigPer')] = 'trace'
+    # initCfg['recordLFP'] = None
+    # initCfg[('analysis', 'plotLFP')] = False
+
+    initCfg['saveCellSecs'] = False
+    initCfg['saveCellConns'] = False
+
+
+    # --------------------------------------------------------
+    # fitness function
+    fitnessFuncArgs = {}
+    pops = {}
+    
+    ## Exc pops
+    Epops = ['IT2', 'IT4', 'IT5A', 'IT5B', 'PT5B', 'IT6', 'CT6']  # all layers
+
+    Etune = {'target': 5, 'width': 20, 'min': 0.05}
+    for pop in Epops:
+        pops[pop] = Etune
+    
+    ## Inh pops 
+    Ipops = ['NGF1',                            # L1
+            'PV2', 'SOM2', 'VIP2', 'NGF2',      # L2/3
+            'PV4', 'SOM4', 'VIP4', 'NGF4',      # L4
+            'PV5A', 'SOM5A', 'VIP5A', 'NGF5A',  # L5A  
+            'PV5B', 'SOM5B', 'VIP5B', 'NGF5B',  # L5B
+            'PV6', 'SOM6', 'VIP6', 'NGF6']       # L6
+
+    Itune = {'target': 10, 'width': 30, 'min': 0.05}
+    for pop in Ipops:
+        pops[pop] = Itune
+    
+    fitnessFuncArgs['pops'] = pops
+    fitnessFuncArgs['maxFitness'] = 1000
+    fitnessFuncArgs['tranges'] = initCfg['printPopAvgRates']
+
+
+    def fitnessFunc(simData, **kwargs):
+        import numpy as np
+        pops = kwargs['pops']
+        maxFitness = kwargs['maxFitness']
+
+        popFitness = [min(np.exp(abs(v['target'] - simData['popRates'][k])/v['width']), maxFitness) 
+                if simData['popRates'][k] > v['min'] else maxFitness for k,v in pops.items()]
+        fitness = np.mean(popFitness)
+
+        popInfo = '; '.join(['%s rate=%.1f fit=%1.f'%(p, simData['popRates'][p], popFitness[i]) for i,p in enumerate(pops)])
+        print('  '+popInfo)
+        return fitness
+        
+    # create Batch object with paramaters to modify, and specifying files to use
+    b = Batch(params=params, netParamsFile='netParams.py', cfgFile='cfg.py', initCfg=initCfg, groupedParams=groupedParams)
+
+    b.method = 'optuna'
+
+    b.optimCfg = {
+        'fitnessFunc': fitnessFunc, # fitness expression (should read simData)
+        'fitnessFuncArgs': fitnessFuncArgs,
+        'maxFitness': fitnessFuncArgs['maxFitness'],
+        'maxiters':     1e6,    #    Maximum number of iterations (1 iteration = 1 function evaluation)
+        'maxtime':      None,    #    Maximum time allowed, in seconds
+        'time_sleep': 10*60, # 5min wait this time before checking again if sim is completed (for each generation)
+        'maxiter_wait': 10*24, # (5h20) max number of times to check if sim is completed (for each generation)
+        'popsize': 1  # unused - run with mpi 
+    }
 
     return b
 
@@ -965,8 +1124,8 @@ def optunaRates(scaleDensity=1.0):
         'maxFitness': fitnessFuncArgs['maxFitness'],
         'maxiters':     1e6,    #    Maximum number of iterations (1 iteration = 1 function evaluation)
         'maxtime':      None,    #    Maximum time allowed, in seconds
-        'maxiter_wait': 16,     # check every time step (time_sleep), 16 times
-        'time_sleep': 900,     # 16 x 900 seconds = 4 hours
+        'time_sleep': 10*60, # 5min wait this time before checking again if sim is completed (for each generation)
+        'maxiter_wait': 10*24, # (5h20) max number of times to check if sim is completed (for each generation)
         'popsize': 1  # unused - run with mpi 
     }
 
@@ -1138,8 +1297,8 @@ def optunaRatesCellTypes():
         'maxFitness': fitnessFuncArgs['maxFitness'],
         'maxiters':     1e6,    #    Maximum number of iterations (1 iteration = 1 function evaluation)
         'maxtime':      None,    #    Maximum time allowed, in seconds
-        'maxiter_wait': 16,     # check every time step (time_sleep), 16 times
-        'time_sleep': 900,     # 16 x 900 seconds = 4 hours
+        'time_sleep': 10*60, # 5min wait this time before checking again if sim is completed (for each generation)
+        'maxiter_wait': 10*24, # (5h20) max number of times to check if sim is completed (for each generation)
         'popsize': 1  # unused - run with mpi 
     }
 
