c8fd421b6a7b4d87bfd0e73f3df54c856df9ac86
Roman Baravalle <romanbaravalle@gmail.com>
2025-07-21 17:37:08 -0500

    updateGPU

--- GPUConfig.sh
+++ GPUConfig.sh
@@ -4,4 +4,5 @@ export PATH=$HOME/neuronGPU/bin:$PATH
 export PYTHONPATH=$HOME/neuronGPU/lib/python:$PYTHONPATH
 export LD_LIBRARY_PATH="/usr/lib64/openmpi/lib/":"/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/compilers/lib"
 cd M1_Manifolds
+export PYTHONPATH=$PYTHONPATH:$PWD # do it in \src and in parent folder
 nrnivmodl -coreneuron mod
