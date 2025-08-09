5014422f1251b99b5c2a6d83c82796f1a3551bea
Roman Baravalle <44242643+RomanB22@users.noreply.github.com>
2025-08-09 12:33:59 -0500

    Update setupLocal.sh

--- setupLocal.sh
+++ setupLocal.sh
@@ -10,8 +10,6 @@ nrnivmodl -coreneuron mod/
 # nohup command
 nohup python -u src/batchNew.py > optunaGPU_SGE.txt & echo $! >> pids.pids
 
-J4PXKKROVTM3R4ELLVAQ3CJCL6OUP2WN
-
 nohup python -u src/batchNew.py > optunaGPU_Expanse.txt & echo $! >> pids.pids
 
 nohup python -u src/batchNew.py > optunaCPU_Expanse.txt & echo $! >> pids.pids
