c3d354e5e9c89fa92ee330a29704e1ff4c9f2be8
Roman Baravalle <romanbaravalle@gmail.com>
2025-08-27 18:21:02 -0500

    update batch

--- src/batch.py
+++ src/batch.py
@@ -10,13 +10,13 @@ PercentageChange = 0.5
 minChg = (1-PercentageChange)
 maxChg = (1+PercentageChange)
 
-params = {'weightLong.TPO': [0.1*minChg, 0.5*maxChg],
-          'weightLong.TVL': [0.1*minChg, 0.5*maxChg],
-          'weightLong.S1': [0.1*minChg, 0.5*maxChg],
-          'weightLong.S2': [0.1*minChg, 0.5*maxChg],
-          'weightLong.cM1': [0.1*minChg, 0.5*maxChg],
-          'weightLong.M2': [0.1*minChg, 0.5*maxChg],
-          'weightLong.OC': [0.1*minChg, 0.5*maxChg],
+params = {'weightLong.TPO': [0.1*minChg, 0.75*maxChg],
+          'weightLong.TVL': [0.1*minChg, 0.75*maxChg],
+          'weightLong.S1': [0.1*minChg, 0.75*maxChg],
+          'weightLong.S2': [0.1*minChg, 0.75*maxChg],
+          'weightLong.cM1': [0.1*minChg, 0.75*maxChg],
+          'weightLong.M2': [0.1*minChg, 0.75*maxChg],
+          'weightLong.OC': [0.1*minChg, 0.75*maxChg],
           'EEGain': [1.*minChg, 1.*maxChg],
           'IEweights.0': [1.*minChg, 1.*maxChg],    ## L2/3+4
           'IEweights.1': [1.*minChg, 1.*maxChg],    ## L5
