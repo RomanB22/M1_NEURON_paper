290c59d89d2da4e32674f420fcc368862e5a3a36
Roman Baravalle <romanbaravalle@gmail.com>
2025-08-27 15:36:12 -0500

    Change setup

--- setupLocal.sh
+++ setupLocal.sh
@@ -4,10 +4,7 @@ export PYTHONPATH=$PYTHONPATH:$PWD # do it in \src and in parent folder
 cd src
 export PYTHONPATH=$PYTHONPATH:$PWD # do it in \src and in parent folder
 cd ..
-
-
 nrnivmodl -coreneuron mod/
-
 
 # nohup command
 nohup python -u src/batchNew.py > optunaGPU_SGE.txt & echo $! >> pids.pids
