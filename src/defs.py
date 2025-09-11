5c4db0fa1f0f4c7504d3f864c54cd480a3c2bed6
Roman Baravalle <romanbaravalle@gmail.com>
2025-09-11 13:35:29 -0500

    Change format to a portable one - not pickle

--- src/defs.py
+++ src/defs.py
@@ -737,9 +737,13 @@ def trimTVLSpikes(spikeList, cfg):
     return trimmedList
 
 def load_umap_results(reg='m1', n_components=2, period='scaled_prep'):
-    import pickle
-    with open(f'./manifolds/{period}/umap_results_n{n_components}_{reg}.pkl', 'rb') as f:
-        loaded_results = pickle.load(f)
+    # import pickle
+    # with open(f'./manifolds/{period}/umap_results_n{n_components}_{reg}.pkl', 'rb') as f:
+    #     loaded_results = pickle.load(f)
+    import numpy as np
+    
+    filename = f'./manifolds/{period}/umap_results_n{n_components}_{reg}.npz'
+    loaded_results = np.load(filename, allow_pickle=True)
 
     loaded_reprs = loaded_results['representations']
     loaded_reds = loaded_results['reductions']
